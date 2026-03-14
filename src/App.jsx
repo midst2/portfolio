e70b47e2745066f06971784859fa6437317b90cf
midst2 <work.amiryunuh@gmail.com>
2026-03-15 01:37:10 +0700

    edit navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Footer from "../sections/Footer";
